e775613ba0f3f0067753b50fddab2c8d3a6485a1
andriodsheesh <degaomchrishian.gmail.com>
2026-05-01 02:58:07 +0800

    fix: correct user deletion method in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -50,7 +50,7 @@ class ProfileController extends Controller
 
         Auth::logout();
 
-        $user->delete();
+        $user->delete($user);
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
