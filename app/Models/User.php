1ff2558aaff7800925ba826509e2346646fa6679
andriodsheesh <degaomchrishian.gmail.com>
2026-05-05 11:05:35 +0800

    feat: extend User model with role management and relationships for classrooms and quizzes

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['firstname', 'lastname', 'email', 'password', 'bio', 'profile_picture'])]
+#[Fillable(['firstname', 'lastname', 'email', 'password', 'bio', 'profile_picture', 'role', 'department'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -22,6 +22,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
     protected function casts(): array
     {
         return [
@@ -31,10 +32,59 @@ class User extends Authenticatable
     }
 
     /**
+     * Check if the user is a teacher.
+     */
+    public function isTeacher(): bool
+    {
+        return $this->role === 'teacher';
+    }
+
+    /**
+     * Check if the user is a student.
+     */
+    public function isStudent(): bool
+    {
+        return $this->role === 'student';
+    }
+
+    /**
      * Get the decks that belong to the user.
      */
     public function decks()
     {
         return $this->hasMany(Deck::class);
+    }
+
+    /**
+     * Classrooms this teacher owns.
+     */
+    public function classroomsAsTeacher()
+    {
+        return $this->hasMany(Classroom::class, 'teacher_id');
+    }
+
+    /**
+     * Classrooms this student is enrolled in.
+     */
+    public function enrolledClassrooms()
+    {
+        return $this->belongsToMany(Classroom::class, 'classroom_student', 'student_id', 'classroom_id')
+                    ->withTimestamps();
+    }
+
+    /**
+     * Quizzes created by this teacher.
+     */
+    public function quizzes()
+    {
+        return $this->hasMany(Quiz::class, 'teacher_id');
+    }
+
+    /**
+     * Quiz attempts made by this student.
+     */
+    public function quizAttempts()
+    {
+        return $this->hasMany(QuizAttempt::class, 'student_id');
     }
 }
