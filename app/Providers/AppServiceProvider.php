4b62a8fe9d189676614394b71940c2c2f2590bf7
andriodsheesh <degaomchrishian.gmail.com>
2026-05-06 00:45:44 +0800

    feat: implement sidebar view composer to display recent decks for authenticated users

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\View;
+use App\Models\Deck;
+use Illuminate\Support\Facades\Auth;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('components.sidebar', function ($view) {
+            // Increase the limit or remove it entirely if you want ALL decks
+            $decks = Auth::check()
+                ? Auth::user()->decks()->latest()->take(15)->get()
+                : collect();
+
+            $view->with('recentDecks', $decks);
+        });
     }
 }
