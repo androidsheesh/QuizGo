047335e38107cbb83f1246c0fbefcacca6568521
andriodsheesh <degaomchrishian.gmail.com>
2026-05-15 11:57:02 +0800

    feat: add observer for QuizAttempt model to handle events

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
+
 class Classroom extends Model
 {
     use HasFactory;
--- app/Models/QuizAttempt.php
+++ app/Models/QuizAttempt.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Observers\QuizAttemptObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
+#[ObservedBy([QuizAttemptObserver::class])]
 class QuizAttempt extends Model
 {
     use HasFactory;
